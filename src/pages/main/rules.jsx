21aa5193f6e83134c04a4c30280a3784cb291ab9
Shreyash Gautam <shreyashgautam2007@gmail.com>
2025-03-22 23:44:54 +0530

    Added login

---
+++ src/pages/main/rules.jsx
@@ -0,0 +1,10 @@
+import ParticlesComponent from '../../components/main/particle';
+
+
+function MainRules(){
+    return(
+        <div><ParticlesComponent id="particles" className="absolute inset-0 -z-10" /></div>
+    )
+}
+
+export default MainRules;
